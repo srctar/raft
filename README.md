18714f609e30c0a2c2ab70d47e5e16379110fd4f
QinYupeng <qinyupeng@lianjia.com>
2018-03-13 10:24:39 +0800

    role

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # 引言
 
 计划学习 `Paxos` , 无奈太过难以理解。 因此通过 `Raft` 辅助理解。
-本工程实现基于 CSDN 翻译版 [《In search of an Understandable Consensus Algorithm (Extended Version)》](https://ramcloud.atlassian.net/wiki/download/attachments/6586375/raft.pdf), 译文见: [RAFT 论文中文翻译(1)](http://blog.csdn.net/luoyhang003/article/details/61915666) 和 [RAFT 论文中文翻译(2)](http://blog.csdn.net/luoyhang003/article/details/61915747)。
+
+本工程理论基于 CSDN 翻译版 [《In search of an Understandable Consensus Algorithm (Extended Version)》](https://ramcloud.atlassian.net/wiki/download/attachments/6586375/raft.pdf), 译文见: [RAFT 论文中文翻译(1)](http://blog.csdn.net/luoyhang003/article/details/61915666) 和 [RAFT 论文中文翻译(2)](http://blog.csdn.net/luoyhang003/article/details/61915747)。
+
+实现基于 [Raft Understandable Distributed Consensus](http://thesecretlivesofdata.com/raft/)
---
+++ src/main/java/com/qyp/raft/ServerRole.java
@@ -0,0 +1,33 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  The ASF licenses
+ * this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.qyp.raft;
+
+/**
+ * RAFT 协议中, 单个节点的状态, 一定是在 {@link ServerRole} 其中之一
+ * 初始状态定为 FOLLOWER.
+ *
+ * @author yupeng.qin
+ * @since 2018-03-12
+ */
+public enum ServerRole {
+
+    FOLLOWER,
+
+    CANDIDATE,
+
+    LEADER;
+}
